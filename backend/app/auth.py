ea36f4bfefc2538eec540aafda357003c27ccce7
Judith-AB <142833346+Judith-AB@users.noreply.github.com>
2026-03-23 15:44:34 +0530

    User registration and login with JWT authentication

--- backend/app/auth.py
+++ backend/app/auth.py
@@ -1,1 +1,21 @@
  
+from passlib.context import CryptContext  #for hashing
+from jose import jwt
+from datetime import datetime, timedelta, timezone
+SECRET_KEY="polo-secret-key"
+ALGORITHM="HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES=30
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+def hash_password(password):
+    return pwd_context.hash(password)
+
+def verify_password(plain,hashed):
+    return pwd_context.verify(plain,hashed)
+
+def create_token(data):
+    to_encode=data.copy()
+    expire = datetime.now(timezone.utc) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp":expire})
+    return jwt.encode(to_encode,SECRET_KEY,algorithm=ALGORITHM)
+def decode_token(token):
+    return jwt.decode(token,SECRET_KEY,algorithms=[ALGORITHM])  #forr decryption-expects a list
--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,30 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,HTTPException
+from pydantic import BaseModel
+from app.auth import hash_password,verify_password,create_token,decode_token
 app=FastAPI()
-@app.get('/')
+fake_db={}
+class RegisterRequest(BaseModel):
+    username: str
+    password: str
+class LoginRequest(BaseModel):
+    username: str
+    password: str
+@app.get("/")
 def root():
-    return{"message":"Chat Backend is live"}
+    return {"message": "Polo is alive"}
+            
+@app.post("/register")
+def register(req:RegisterRequest):
+    if req.username in fake_db:
+        raise HTTPException(status_code=400, detail="Username already exists")
+    fake_db[req.username] = hash_password(req.password)
+    return {"message": "User registered successfully"}
 
- 
+@app.post("/login")
+def login(req:LoginRequest):
+    if req.username not in fake_db:
+         raise HTTPException(status_code=400, detail="User not found") 
+    if not verify_password(req.password,fake_db[req.username]):
+        raise HTTPException(status_code=400, detail="Wrong Password") 
+    token = create_token({"sub": req.username})
+    return {"access_token": token, "token_type": "bearer"}
