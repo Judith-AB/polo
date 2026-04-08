afe194b7512700c3c0bf026c25f2dabbe7d2fc5e
Judith-AB <142833346+Judith-AB@users.noreply.github.com>
2026-04-08 20:10:12 +0530

    postgresql connected, users, messages tables created

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,1 +1,18 @@
  
+from sqlalchemy import Column, Integer, String, DateTime
+from datetime import datetime, timezone
+from app.database import Base
+class User(Base):
+    __tablename__='users'
+    id=Column(Integer,primary_key=True,autoincrement=True)
+    username=Column(String,unique=True)
+    password=Column(String)
+class Message(Base):
+    __tablename__='messages'
+    id=Column(Integer,primary_key=True,autoincrement=True)
+    room_id=Column(String)
+    username=Column(String)
+    content=Column(String)
+    status =Column(String)
+    timestamp = Column(DateTime, default=lambda: datetime.now(timezone.utc))
+
